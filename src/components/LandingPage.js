f1b54fd3dc17c352e1ac9e16666a1ea960b27cf3
jayashreeodela <110324547+jayashreeodela@users.noreply.github.com>
2024-12-21 19:08:41 +0530

    Add files via upload

---
+++ src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="landing">
+      <h1>Welcome to CollabTool</h1>
+      <p>
+        {/* A real-time collaboration tool that empowers you to work together seamlessly
+        with dynamic updates and document sharing. Sign up now to get started! */}
+
+        Experience the Power of real-time collaboration - where ideas flow freely, and progress knows no bounds.. 
+      </p>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// Inside NavBar.js
+import './NavBar.css';
+
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">CollabTool</h1>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
