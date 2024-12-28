13c7659bcd054e02866146bd139b3e511d02f70d
jayashreeodela <110324547+jayashreeodela@users.noreply.github.com>
2024-12-28 22:41:34 +0530

    Add files via upload

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h2>Welcome to the Dashboard!</h2>
+      {/* Add any content you want to display on the dashboard */}
+    </div>
+  );
+}
+
+export default Dashboard;
