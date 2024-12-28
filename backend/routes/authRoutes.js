81f951d3b2b129f144f232d2c341c233cbfb2ba5
jayashreeodela <110324547+jayashreeodela@users.noreply.github.com>
2024-12-28 22:20:56 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
-
+const { registerUser, loginUser } = require('../controllers/authController');
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+// Route for user registration
+router.post('/register', registerUser);
+
+// Route for user login
+router.post('/login', loginUser);
 
 module.exports = router;
+
+
+// const express = require('express');
+// const { register, login } = require('../controllers/authController');
+
+// const router = express.Router();
+
+// router.post('/register', register);
+// router.post('/login', login);
+
+// module.exports = router;
