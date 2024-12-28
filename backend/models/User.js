b9bcd374f0a50303d7d95f5b6d286eea3754c449
jayashreeodela <110324547+jayashreeodela@users.noreply.github.com>
2024-12-28 22:58:13 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -24,23 +24,3 @@ const UserSchema = new mongoose.Schema(
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
-
-
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-
-
-// const UserSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-// }, { timestamps: true });
-
-// UserSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) return next();
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-//   next();
-// });
-
-// module.exports = mongoose.model('User', UserSchema);
