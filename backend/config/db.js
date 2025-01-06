08a875d442f7911301afdadea3acd5b7de20f653
jayashreeodela <110324547+jayashreeodela@users.noreply.github.com>
2025-01-06 07:05:28 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,19 +13,19 @@ const connectDB = async () => {
 module.exports = { connectDB };
 
 
+
 // const mongoose = require('mongoose');
 
 // const connectDB = async () => {
 //     try {
-//         const conn = await mongoose.connect(process.env.MONGO_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         });
+//         const conn = await mongoose.connect(process.env.MONGO_URI);
 //         console.log(`MongoDB connected: ${conn.connection.host}`);
 //     } catch (error) {
 //         console.error(`Error: ${error.message}`);
-//         process.exit(1); // Exit the process if DB connection fails
+//         process.exit(1);
 //     }
 // };
 
 // module.exports = { connectDB };
+
+
