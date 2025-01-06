3f4819087d3cd51a409b61d565d914ab54d8d13d
jayashreeodela <110324547+jayashreeodela@users.noreply.github.com>
2025-01-06 07:20:16 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,31 +25,3 @@ function App() {
 }
 
 export default App;
-
-
-
-// import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import NavBar from './components/NavBar';
-// import LandingPage from './components/LandingPage';
-// import Registration from './components/Registration';
-// import Login from './components/Login';
-// import Footer from './components/Footer';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <NavBar />
-//         <Routes>
-//           <Route path="/" element={<LandingPage />} />
-//           <Route path="/register" element={<Registration />} />
-//           <Route path="/login" element={<Login />} />
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
